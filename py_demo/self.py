2b9e8582547b24890e939c18d59076f9ff3c8915
JinHemu <490754775@qq.com>
2024-07-31 18:54:54 +0800

    使用cmake导入urdf模型文件时,需要install让

---
+++ py_demo/self.py
@@ -0,0 +1,26 @@
+class me:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.c = 3
+        self.d = 4
+        self.e = 5
+        self.f = 6
+        self.g = 7
+        self.h = 8
+        self.i = 9
+        self.j = 10
+        self.k = 11
+        self.l = 12
+        self.m = 13
+        self.n = 14
+        self.o = 15
+        self.p = 16
+        self.q = 17
+        self.r = 18
+        self.s = 19
+
+
+if __name__ == '__main__':
+    wo = me()
+    print(wo.a)
